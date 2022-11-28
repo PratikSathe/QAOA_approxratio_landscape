a177293a0087ecc59b721b503eb1ce9be7e3cb22
Pratik Sathe <sathepratik@gmail.com>
2022-11-28 13:23:29 -0800

    added metrics computation

--- methods.py
+++ methods.py
@@ -1,8 +1,13 @@
 
 from qiskit import QuantumCircuit, Aer, execute
+import numpy as np
+import math
+import networkx as nx
+import matplotlib.pyplot as plt
+from itertools import product as iterprod
 
 def get_size_dist(counts, sizes):
-    """ For given measurement outcomes, i.e. combinations of cuts, counts and sizes, return counts corresponding to each cut size.
+    """ For given measurement outcomes, i.e. combinations of counts and sizes, return counts corresponding to each cut size.
     Args:
         counts (list): List of integers, denoting number of times each cut was measured 
         sizes (list): List of integers. Cut size corresponding to each measured cut
@@ -25,11 +30,113 @@ def get_size_dist(counts, sizes):
     unique_counts = [x[1] for x in s_and_c_list]
     return unique_counts, unique_sizes
     
+def compute_energy_expectation(counts, sizes, **kwargs):
+    """
+    Compute the mean of cut sizes
+    This approximates the expectation value of energy
+
+    Parameters
+    ----------
+    counts : ndarray of ints
+        measured counts corresponding to cuts
+    sizes : ndarray of ints
+        cut sizes (i.e. number of edges crossing the cut)
+    **kwargs : optional arguments
+        will be ignored
+
+    Returns
+    -------
+    float : energy expectation
+    """
+    # Convert counts and sizes to ndarrays, if they are lists
+    counts, sizes = np.array(counts), np.array(sizes)
+
+    return np.sum(counts * sizes) / np.sum(counts)
+
+def compute_cvar(counts, sizes, alpha = 0.1, **kwargs):
+    """
+    Obtains the Conditional Value at Risk or CVaR for samples measured at the end of the variational circuit.
+    Reference: Barkoutsos, P. K., Nannicini, G., Robert, A., Tavernelli, I. & Woerner, S. Improving Variational Quantum Optimization using CVaR. Quantum 4, 256 (2020).
+
+    Parameters
+    ----------
+    counts : ndarray of ints
+        measured counts corresponding to cuts
+    sizes : ndarray of ints
+        cut sizes (i.e. number of edges crossing the cut)
+    alpha : float, optional
+        Confidence interval value for CVaR. The default is 0.1.
+
+    Returns
+    -------
+    float
+        CVaR value
+
+    """
+    # Convert counts and sizes to ndarrays, if they are lists
+    counts, sizes = np.array(counts), np.array(sizes)
+    
+    # Sort the negative of the cut sizes in a non-decreasing order.
+    # Sort counts in the same order as sizes, so that i^th element of each correspond to each other
+    sort_inds = np.argsort(-sizes)
+    sizes = sizes[sort_inds]
+    counts = counts[sort_inds]
+
+    # Choose only the top num_avgd = ceil(alpha * num_shots) cuts. These will be averaged over.
+    num_avgd = math.ceil(alpha * np.sum(counts))
+
+    # Compute cvar
+    cvar_sum = 0
+    counts_so_far = 0
+    for c, s in zip(counts, sizes):
+        if counts_so_far + c >= num_avgd:
+            cts_to_consider = num_avgd - counts_so_far
+            cvar_sum += cts_to_consider * s
+            break
+        else:
+            counts_so_far += c
+            cvar_sum += c * s
+
+    return cvar_sum / num_avgd
+
+def compute_gibbs(counts, sizes, eta = 0.5, **kwargs):
+    """
+    Compute the Gibbs objective function for given measurements
+
+    Parameters
+    ----------
+    counts : ndarray of ints
+        measured counts corresponding to cuts
+    sizes : ndarray of ints
+        cut sizes (i.e. number of edges crossing the cut)
+    eta : float, optional
+        Inverse Temperature
+    Returns
+    -------
+    float
+        Gibbs objective function value
+
+    """
+    # Convert counts and sizes to ndarrays, if they are lists
+    counts, sizes = np.array(counts), np.array(sizes)
+    ls = max(sizes)#largest size
+    shifted_sizes = sizes - ls
+    
+    # gibbs = - np.log( np.sum(counts * np.exp(eta * sizes)) / np.sum(counts))
+    gibbs = - eta * ls - np.log(np.sum (counts / np.sum(counts) * np.exp(eta * shifted_sizes)))
+    return gibbs
+
+
+def compute_best_cut_from_measured(counts, sizes, **kwargs):
+    """
+    From the measured cuts, return the size of the largest cut
+    """
+    return np.max(sizes)
     
 class qaoa_anstaz:
-    def __init__(self, nodes, edges, betas, gammas, num_shots):
+    def __init__(self, nodes, edges, betas, gammas, num_shots = 5000, eta = 0.5, optimal_cut = [], optimal_size = -1):
         """
-        Initialize a class for a QAOA ansatz circuit
+        Initialize a class for a QAOA ansatz circuit. Create corresponding circuit as well.
 
         Args:
             nodes (int): number of nodes in the graph
@@ -43,26 +150,51 @@ class qaoa_anstaz:
         self.betas = betas
         self.gammas = gammas
         self.num_shots = num_shots
+        self.eta = eta
+        self.nxgraph = nx.from_edgelist(edges)
         
         assert len(self.betas) == len(self.gammas), "Betas and Gammas lists should be of the same length"
         self.rounds = len(self.betas) 
         
-        self.create_ansatz_circuit()
+        if len(optimal_cut) == 0 or optimal_size == -1:
+            # Compute max cut using a brute force approach.
+            self.brute_force_compute_maxcut()
+        else: 
+            # Provided values will be assumed to be correct.
+            self.optimal_cut = optimal_cut
+            self.optimal_size = optimal_size
         
-    def eval_cut(self, solution):
+        self.create_ansatz_circuit()
+    
+    def draw_graph(self):
+        "Draw graph"
+        nx.draw(self.nxgraph)
+    
+    def draw_ansatz(self):
+        self.circuit.draw(output='mpl')
+    
+    
+    def brute_force_compute_maxcut(self):
+        """Compute the max cut size and (one of possibly multiple) max cut.
+        Compute classical using a brute force approach, going through all possible cuts.
+        """
+        list_0_1s = [[0]] + [[0,1]] * (self.nodes-1)
+        
+        cut_list = list(iterprod(*list_0_1s))
+        cut_size_list = [self.eval_cut(cut) for cut in cut_list]
+        self.optimal_size = max(cut_size_list)
+        self.optimal_cut = cut_list[cut_size_list.index(self.optimal_size)] # Only one optimal cut will be stored
+        
+    def eval_cut(self, cut):
         """Compute the cut size corresponding to a given solution
 
         Args:
-            solution : List of 0s and 1s, denoting a cut
+            cut : List of 0s and 1s, denoting a cut
 
         Returns:
             int: cut size
         """
-        cut_size = 0
-        for i,j in self.edges:
-            if solution[i] != solution[j]:
-                cut_size += 1
-
+        cut_size = len([1 for (i,j) in self.edges if cut[i]!=cut[j]])
         return cut_size
         
     def execute_circuit(self):
@@ -82,7 +214,19 @@ class qaoa_anstaz:
         self.counts = list(cuts_counts_dict.values())
         self.sizes = [self.eval_cut(solution[::-1]) for solution in self.cuts] # Reverse each cut passed ot eval_cut, since qiskit uses the little-endian format
         
-    
+        self.unique_counts, self.unique_sizes = get_size_dist(self.counts, self.sizes)
+        
+        self.compute_all_metrics()
+        
+    def compute_all_metrics(self):
+        self.best_measured_size = compute_best_cut_from_measured(self.counts, self.sizes)
+        self.gibbs = compute_gibbs(self.counts, self.sizes, self.eta)
+        self.cvar = compute_cvar(self.counts, self.sizes, self.alpha)
+        self.energy_expectation = compute_energy_expectation(self.counts, self.sizes)
+        
+        self.bestcut_ratio = self.best_measured_size / self.optimal_size
+        
+
     def create_ansatz_circuit(self):
         """
         Create the ansatz circuit (determined by the β and γ parameters)
